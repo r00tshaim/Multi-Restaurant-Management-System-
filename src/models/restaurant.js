a7971610f23d8e861013fee594f4ed3e9da8e7c2
AKBARHUSAIN PATEL <akbar.p@upsquare.in>
2021-01-26 13:20:20 +0530

    Restructered code and added menu in the schema

---
+++ src/models/restaurant.js
@@ -0,0 +1,25 @@
+const { Menu } = require("@material-ui/core");
+var mongoose = require("mongoose");
+
+// SCHEMA SETUP
+var restaurantSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  logo: String,
+  address: String,
+  openingTime: String,
+  closingTime: String,
+  takingOrders: Boolean,
+  menu: [
+    {
+      name: String,
+      price: Number,
+      available: Boolean,
+      category: ["Starter", "Main Course"],
+    },
+  ],
+});
+
+const menuSchema = new mongoose.Schema();
+
+module.exports = mongoose.model("Restaurant", restaurantSchema);
